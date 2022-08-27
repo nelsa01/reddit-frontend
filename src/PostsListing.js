f51d13f45b05d40e8d254149929fdc6f9185c0d0
nelsa01 <n.ishimwe@alustudent.com>
2022-08-27 09:49:26 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/PostsListing.js
@@ -0,0 +1,24 @@
+import Post from "./Post";
+import {useState,useEffect} from "react";
+import axios from "axios";
+
+function PostsListing() {
+  const [comments,setComments] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://backendreddit.herokuapp.com/comments', {withCredentials:true})
+      .then(response => setComments(response.data));
+
+  }, []);
+
+
+  return (
+    <div className="bg-reddit_dark">
+      {comments.map(comment => (
+        <Post {...comment} isListing={true} />
+      ))}
+    </div>
+  );
+}
+
+export default PostsListing;
---
+++ src/SearchResultsPage.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Post from "./Post";
+
+function SearchResultsPage(props) {
+  const {text} = props.match.params;
+  const [comments,setComments] = useState([]);
+  useEffect(() => {
+    axios.get('https://backendreddit.herokuapp.com/comments?search='+text, {withCredentials:true})
+      .then(response => setComments(response.data));
+  }, []);
+
+
+  return (
+    <div className="bg-reddit_dark">
+      {comments.map(comment => (
+        <Post {...comment} isListing={true} />
+      ))}
+    </div>
+  );
+}
+
+export default SearchResultsPage;
